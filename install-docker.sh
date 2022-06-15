0f8ba1b67a6588055d2a3c5cbd6d654cd3445af7
pavlenkoia <pavlenkoia@gmail.com>
2022-06-15 15:04:46 +0500

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -10,7 +10,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 
 sudo apt update
 
-apt-cache policy docker-ce
+#apt-cache policy docker-ce
 
 sudo apt install docker-ce
 
