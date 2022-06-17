c0d167b45c357624263bb0667ad43ddc5ef6d9b8
pavlenkoia <pavlenkoia@gmail.com>
2022-06-17 17:22:08 +0500

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
